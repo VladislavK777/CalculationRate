611e82238c197876d4d0a0399cea66632b0b530d
Vladislav.Klochkov <Vladislav.Klochkov@billing.ru>
2019-02-14 15:31:06 +0300

    update_14.02.2019

--- src/test/java/com/uraltranscom/calculaterate/dao/GetTotalModelDAOTest.java
+++ src/test/java/com/uraltranscom/calculaterate/dao/GetTotalModelDAOTest.java
@@ -1,37 +1,38 @@
-/*
 package com.uraltranscom.calculaterate.dao;
 
-import com.uraltranscom.calculaterate.configuration.AppInitTest;
+import com.uraltranscom.calculaterate.configuration.AppConfig;
+import com.uraltranscom.calculaterate.configuration.AppInit;
+import com.uraltranscom.calculaterate.configuration.DBConfig;
 import com.uraltranscom.calculaterate.model_ex.TotalModel;
 import com.uraltranscom.calculaterate.util.PrepareMapParams;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
-*/
 /**
  * @author Vladislav.Klochkov
  * @project CalculationRate_1.0
  * @date 13.02.2019
- *//*
+ */
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = AppInitTest.class)
+@ContextConfiguration(classes = {
+        AppInit.class,
+        DBConfig.class,
+        AppConfig.class})
+@WebAppConfiguration
 public class GetTotalModelDAOTest {
 
-    @Qualifier("getTotalModelDAO")
     @Autowired
     private GetTotalModelDAO getTotalModelDAO;
-
     private TotalModel totalModel;
 
     @Test
     public void testGetTotalModel() {
-        totalModel = getTotalModelDAO.getObject(PrepareMapParams.prepareMapWithParams("190900", "191208", "023002", "120"));
+        totalModel = getTotalModelDAO.getObject(PrepareMapParams.prepareMapWithParams("190900", "191208", "023002", 120));
         System.out.println(totalModel);
     }
 }
-*/
