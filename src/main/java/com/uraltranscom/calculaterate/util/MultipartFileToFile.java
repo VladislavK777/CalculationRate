8cdcf8014b881d76ccd27cbd30e01578c1ee82f0
vladislavK777 <vlad-klochkov@mail.ru>
2019-03-18 19:21:29 +0300

    update_18.03.2019 - Fixed problem download file

--- src/main/java/com/uraltranscom/calculaterate/util/MultipartFileToFile.java
+++ src/main/java/com/uraltranscom/calculaterate/util/MultipartFileToFile.java
@@ -18,25 +18,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class MultipartFileToFile {
-    // Подключаем логгер
     private static Logger logger = LoggerFactory.getLogger(MultipartFileToFile.class);
-
     public static File convertToFile;
 
-    public static File multipartToFile(MultipartFile multipart) {
-        try {
-            convertToFile = new File(System.getProperty("java.io.tmpdir"), multipart.getOriginalFilename());
-            convertToFile.createNewFile();
-            try(FileOutputStream fileOutputStream = new FileOutputStream(convertToFile)) {
-                fileOutputStream.write(multipart.getBytes());
-            }
-        } catch (IOException e) {
-            logger.error("Ошибка конвертации файла - {}", e.getMessage());
-        }
+    public static File multipartToFile(File file) {
+        convertToFile = new File(System.getProperty("java.io.tmpdir"), file.getName());
         return convertToFile;
     }
 }
---
+++ src/test/java/com/uraltranscom/calculaterate/util/MultipartFileToFileTest.java
@@ -0,0 +1,24 @@
+/*
+package com.uraltranscom.calculaterate.util;
+
+import org.junit.Test;
+
+import java.io.File;
+
+*/
+/**
+ * @author Vladislav Klochkov
+ * @create 2019-03-18
+ *//*
+
+
+public class MultipartFileToFileTest {
+
+    private File file = new File("/Macintosh HD/Пользователи/vladislavklockov/Рабочий стол/template.xlsx");
+
+    @Test
+    public void test() {
+        MultipartFileToFile.multipartToFile(file);
+    }
+}
+*/
